24488f71841fc760170a2d2aa950a60bd95f252a
sophia <sophiakim.dev@gmail.com>
2024-10-02 18:29:20 -0400

    created containers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,26 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      
+      <div className="container">
+        <div className="profileContainer">
+          <div className="profileImage">
+            <Image
+              src="/asset/23.jpg"
+              alt="TechTank Logo"
+              width={500}
+              height={500}
+            />
+          </div>
+          <div className="profile">
+            <div className="name">
+              <h1>TechTank</h1>
+            </div>
+            <p>description</p>
+          </div>
+        </div>
+        <div className="socialContainer"></div>
+        <div className="mainContainer"></div>
+      </div>
     </div>
   );
 }

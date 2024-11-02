994b4efff88c31f89b7426214b8419975e4d1824
sophia <sophiakim.dev@gmail.com>
2024-11-01 23:43:45 -0400

    added icon and image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faSpotify } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function Home() {
   return (
@@ -8,20 +14,29 @@ export default function Home() {
         <div className="profileContainer">
           <div className="profileImage">
             <Image
-              src="/asset/23.jpg"
+              src="/asset/21.jpg"
               alt="TechTank Logo"
-              width={500}
-              height={500}
+              width={300}
+              height={300}
             />
           </div>
           <div className="profile">
             <div className="name">
               <h1>TechTank</h1>
             </div>
-            <p>description</p>
+            <p>
+              Welcome to TechTank |📍Toronto, ON A diverse hub for all tech
+              enthusiasts!
+            </p>
           </div>
         </div>
-        <div className="socialContainer"></div>
+        <div className="socialContainer">
+          <FontAwesomeIcon icon={faInstagram} className="small-icon" />
+          <FontAwesomeIcon icon={faYoutube} className="small-icon" />
+          <FontAwesomeIcon icon={faLinkedinIn} className="small-icon" />
+          <FontAwesomeIcon icon={faSpotify} className="small-icon" />
+          <FontAwesomeIcon icon={faGithub} className="small-icon" />
+        </div>
         <div className="mainContainer"></div>
       </div>
     </div>
